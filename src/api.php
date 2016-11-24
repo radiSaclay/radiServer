8266f6cee53503e4efaf2308fedc42908e6e2f78
Brendan <Granipouss@gmail.com>
2016-11-24 15:17:28 +0100

    get all objects by name

--- src/api.php
+++ src/api.php
@@ -2,16 +2,26 @@
 
 // Get query objects
 function query ($table) {
+  if ($table == 'pin') return \PinQuery::create();
+  if ($table == 'user') return \UserQuery::create();
   if ($table == 'farms') return \FarmQuery::create();
+  if ($table == 'order') return \OrderQuery::create();
   if ($table == 'events') return \EventQuery::create();
   if ($table == 'products') return \ProductQuery::create();
+  if ($table == 'farm-product') return \FarmProductQuery::create();
+  if ($table == 'subscription') return \SubscriptionQuery::create();
 }
 
 // Get instance objects
 function instanciate ($table) {
+  if ($table == 'pin') return new \Pin();
+  if ($table == 'user') return new \User();
   if ($table == 'farms') return new \Farm();
+  if ($table == 'order') return new \Order();
   if ($table == 'events') return new \Event();
   if ($table == 'products') return new \Product();
+  if ($table == 'farm-product') return new \FarmProduct();
+  if ($table == 'subscription') return new \Subscription();
 }
 
 // ==================================================
