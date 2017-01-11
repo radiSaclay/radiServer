19ecc6fb012ede9df0b1aa92ec6067590318854d
Tiberio <tiberiusferreira@gmail.com>
2017-01-11 01:05:07 +0100

    Fix indentiation

--- routes/api/events.php
+++ routes/api/events.php
@@ -5,12 +5,12 @@
 // Returns the event id
 // ==================================================
 $app->get('/api/events/{id}', function ($request, $response, $args) {
-    $event = EventQuery::create()->findPK($args['id']);
-    if ($event) {
-        return $response->withJson(return_event($event, $request), 200);
-    }else{
-        return $response->withJson(["Error" => "Event not found"], 404);
-    }
+  $event = EventQuery::create()->findPK($args['id']);
+  if ($event) {
+    return $response->withJson(return_event($event, $request), 200);
+  }else{
+    return $response->withJson(["Error" => "Event not found"], 404);
+  }
 
 });
 
@@ -22,33 +22,33 @@ $app->get('/api/events/{id}', function ($request, $response, $args) {
 // having pinned or not the event
 // ==================================================
 $app->get('/api/events/', function ($request, $response) {
-    // If the request comes from a farmer, $events = auth\getFarm($request)->getEvents() (Farm events)
-    // Else EventQuery::create()->find() (All events)
-    $events = auth\isFarmer($request)
-        ? auth\getFarm($request)->getEvents()
-        : EventQuery::create()->find();
+  // If the request comes from a farmer, $events = auth\getFarm($request)->getEvents() (Farm events)
+  // Else EventQuery::create()->find() (All events)
+  $events = auth\isFarmer($request)
+    ? auth\getFarm($request)->getEvents()
+    : EventQuery::create()->find();
 
-    return api\mapCollection(
-        $response, $events,
-        function ($event) use ($request) {
-            return return_event($event, $request);
-        }
-    );
+  return api\mapCollection(
+    $response, $events,
+    function ($event) use ($request) {
+      return return_event($event, $request);
+    }
+  );
 });
 
 function return_event($event, $request) {
-    $data = $event->serialize();
-    $data["farmName"] = $event->getFarm()->getName();
-    $data["prodName"] = $event->getProduct()->getName();
-    // if it is farmer requesting add number of users to each event
-    if (auth\isFarmer($request)) {
-        $data["pins"] = $event->countUsers();
-    } else if (auth\isUser($request)) {
-        // if it is user flag the events it pinned
-        $user = auth\getUser($request);
-        $data["pinned"] = $event->getUsers()->contains($user);
-    }
-    return $data;
+  $data = $event->serialize();
+  $data["farmName"] = $event->getFarm()->getName();
+  $data["prodName"] = $event->getProduct()->getName();
+  // if it is farmer requesting add number of users to each event
+  if (auth\isFarmer($request)) {
+    $data["pins"] = $event->countUsers();
+  } else if (auth\isUser($request)) {
+    // if it is user flag the events it pinned
+    $user = auth\getUser($request);
+    $data["pinned"] = $event->getUsers()->contains($user);
+  }
+  return $data;
 }
 
 // ==================================================
@@ -58,13 +58,13 @@ function return_event($event, $request) {
 // and puts it in the DB
 // ==================================================
 $app->post('/api/events/', function ($request, $response) {
-    try {
-        $event = new Event();
-        $event->setFarmId(auth\getFarm($request)->getId());
-        return api\update($request, $response, $event);
-    } catch (Exception $e) {
-        return $response->withStatus(400);
-    }
+  try {
+    $event = new Event();
+    $event->setFarmId(auth\getFarm($request)->getId());
+    return api\update($request, $response, $event);
+  } catch (Exception $e) {
+    return $response->withStatus(400);
+  }
 })->add('mwIsFarmer');
 
 // ==================================================
@@ -73,16 +73,16 @@ $app->post('/api/events/', function ($request, $response) {
 // If event exists it updates it in DB
 // ==================================================
 $app->put('/api/events/{id}', function ($request, $response, $args) {
-    $event = EventQuery::create()->findPK($args['id']);
-    if ($event == null) return $response->withStatus(404);
-    if ($event->getFarmId() != auth\getFarm($request)->getId()) {
-        return $response->withStatus(401);
-    }
-    try {
-        return api\update($request, $response, $event);
-    } catch (Exception $e) {
-        return $response->withStatus(400);
-    }
+  $event = EventQuery::create()->findPK($args['id']);
+  if ($event == null) return $response->withStatus(404);
+  if ($event->getFarmId() != auth\getFarm($request)->getId()) {
+    return $response->withStatus(401);
+  }
+  try {
+    return api\update($request, $response, $event);
+  } catch (Exception $e) {
+    return $response->withStatus(400);
+  }
 })->add('mwIsFarmer');
 
 
@@ -90,17 +90,17 @@ $app->put('/api/events/{id}', function ($request, $response, $args) {
 // > DELETE /api/events/
 // ==================================================
 $app->delete('/api/events/{id}', function ($request, $response, $args) {
-    $event = EventQuery::create()->findPK($args['id']);
-    if ($event == null) return $response->withStatus(404);
-    if ($event->getFarmId() != auth\getFarm($request)->getId()) {
-        return $response->withStatus(401);
-    }
-    try {
-        $event->delete();
-        return $response->withStatus(200);
-    } catch (Exception $e) {
-        return $response->withStatus(400);
-    }
+  $event = EventQuery::create()->findPK($args['id']);
+  if ($event == null) return $response->withStatus(404);
+  if ($event->getFarmId() != auth\getFarm($request)->getId()) {
+    return $response->withStatus(401);
+  }
+  try {
+    $event->delete();
+    return $response->withStatus(200);
+  } catch (Exception $e) {
+    return $response->withStatus(400);
+  }
 })->add('mwIsFarmer');
 
 // ==================================================
@@ -108,30 +108,30 @@ $app->delete('/api/events/{id}', function ($request, $response, $args) {
 // Adds user to event (pins it)
 // ==================================================
 $app->post('/api/events/pin/{id}', function ($request, $response, $args) {
-    $user = auth\getUser($request);
-    $event = EventQuery::create()->findPK($args['id']);
-    if ($event == null) return $response->withStatus(404);
-    try {
-        $event->addUser($user);
-        $event->save();
-        return $response->withStatus(200);
-    } catch (Exception $e) {
-        return $response->withStatus(400);
-    }
+  $user = auth\getUser($request);
+  $event = EventQuery::create()->findPK($args['id']);
+  if ($event == null) return $response->withStatus(404);
+  try {
+    $event->addUser($user);
+    $event->save();
+    return $response->withStatus(200);
+  } catch (Exception $e) {
+    return $response->withStatus(400);
+  }
 })->add('mwIsLogged');
 
 // ==================================================
 // > POST /api/events/unpin/{id}
 // ==================================================
 $app->post('/api/events/unpin/{id}', function ($request, $response, $args) {
-    $user = auth\getUser($request);
-    $event = EventQuery::create()->findPK($args['id']);
-    if ($event == null) return $response->withStatus(404);
-    try {
-        $event->removeUser($user);
-        $event->save();
-        return $response->withStatus(200);
-    } catch (Exception $e) {
-        return $response->withStatus(400);
-    }
+  $user = auth\getUser($request);
+  $event = EventQuery::create()->findPK($args['id']);
+  if ($event == null) return $response->withStatus(404);
+  try {
+    $event->removeUser($user);
+    $event->save();
+    return $response->withStatus(200);
+  } catch (Exception $e) {
+    return $response->withStatus(400);
+  }
 })->add('mwIsLogged');
