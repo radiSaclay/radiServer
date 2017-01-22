c0d9f07064c40c5f9add445fd1ec049ed9d7c8af
Tiberio <tiberiusferreira@gmail.com>
2017-01-22 03:54:03 +0100

    runTests now automatically switches between Databases

--- tools/tests/runTests.sh
+++ tools/tests/runTests.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 set -e
+initial_dir=`pwd`
+./setupTestDB.sh
 cd ../../src/tests/
+echo ""
+echo "Running tests..."
+echo ""
 phpunit authTest.php
+cd $initial_dir
+./RevertToOriginalDB.sh
